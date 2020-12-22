428364624fa897b4ac785fe471ba364810c16bff
Abishek G <abishekg2712@gmail.com>
2020-12-22 16:17:02 +0530

    Initial Commit

---
+++ frontend/components/Page.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Page extends Component {
+    render() {
+        return (
+            <div>
+                <p>Hey I am the Page component</p>
+            </div>
+        )
+    }
+}
+
+export default Page;
